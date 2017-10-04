50c564789516d5ace4bf38eb51e7254aa5b0259f
Wil Seely-Kirk <wil.seely-kirk@sap.com>
2017-10-04 10:06:00 -0700

    minor changes

--- stage/services/modular-k8s-nodes/main.tf
+++ stage/services/modular-k8s-nodes/main.tf
@@ -1,14 +1,14 @@
 provider "google" {
-  project     = "${var.project}"
-  region      = "${var.region}"
+  project = "${var.project}"
+  region  = "${var.region}"
 }
 
 #configure remote state
 
 terraform {
   backend "gcs" {
-    bucket = "glds-k8s-remote-state-stage"
-    path = "k8s_nodes_stage/terraform.tfstate"
+    bucket  = "glds-k8s-remote-state-stage"
+    path    = "k8s_nodes_stage/terraform.tfstate"
     project = "glds-gcp"
   }
 }
@@ -16,13 +16,13 @@ terraform {
 #Create the VMs
 
 module "k8s" {
-  source = "github.com/GoogleCloudPlatform/terraform-google-k8s-gce"
-  network = "tf-k8s-staging" 
-  subnetwork = "k8s-subnet-stage"
-  name = "k8s-staging"
+  source      = "github.com/GoogleCloudPlatform/terraform-google-k8s-gce"
+  network     = "tf-k8s-staging"
+  subnetwork  = "k8s-subnet-stage"
+  name        = "k8s-staging"
   k8s_version = "1.7.3"
-  region = "${var.region}"
-  zone = "us-west1-a"
-  num_nodes = "3"
-  master_ip = "10.240.0.10"
+  region      = "${var.region}"
+  zone        = "us-west1-a"
+  num_nodes   = "3"
+  master_ip   = "10.240.0.10"
 }
